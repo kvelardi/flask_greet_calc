5a43f81a7755b67cd06d6a22076e9b51f34e98bc
Kristen V <124750746+kvelardi@users.noreply.github.com>
2024-01-31 20:16:34 -0500

    finalized code for greeting app

--- greet/app.py
+++ greet/app.py
@@ -1,18 +1,22 @@
-
+# Make a simple Flask app that responds to these routes with simple text:
 from flask import Flask
 
-app = Flask(__name__)
+app= Flask(__name__)
 
+# /welcome
+# returns "welcome"
 @app.route('/welcome')
 def welcome():
     return "welcome"
-
+  
+# /welcome/home
+# returns "welcome home"
 @app.route('/welcome/home')
 def welcome_home():
     return "welcome home"
 
+# /welcome/back
+# returns "welcome back"
 @app.route('/welcome/back')
 def welcome_back():
     return "welcome back"
-
-
